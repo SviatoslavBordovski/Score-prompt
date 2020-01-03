5be2fad10f71a841c87b3ecfaf0112636575e867
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-01-03 15:26:54 +0200

    Create score.py

---
+++ score.py
@@ -0,0 +1,14 @@
+score = input("Enter Score: ")
+
+scr = float(score)
+
+if scr >= 0.9:
+   print('A')
+elif scr >= 0.85:
+   print('B')
+elif scr >= 0.7:
+   print('C')
+elif scr >= 0.6:
+   print('D')
+elif scr < 1.0:
+   print('F')
